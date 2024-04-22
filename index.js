daba702634dcb19192fd119af640a99950950a3e
CharlyS <charlysandoval331@gmail.com>
2024-04-22 18:04:06 -0500

    server is now running and server now connects to database. Also note, there is a e text document that omits .env from being commited to code echo .env >> .gitignore

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const mongoString = process.env.DATABASE_URL
+
+// connecting to the database
+mongoose.connect( mongoString)
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+database.once('connected', () => {
+    console.log('Database Connected')
+})
+
+
+
+const app = express()
+
+// - express.json() and express.urlencoded() are middlewares (methods/functions/operation) that are called
+//   between processing the request and sending a response in your app
+// - express.json() is a middleware that acts on post/put request. It processes the req.body of post/put request
+//      > it will parse req.body as json object
+// - express.urlencoded() will parse req.body as ascii chararcter
+// # project idea - parse req.body manually and build your own body parser
+app.use(express.json() )
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
+
+
+app.listen(3000, () => {
+    console.log(`Server starter on port ${3000}`)
+})
